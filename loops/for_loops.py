6df2685ade546882f9c0071e888e5cfbd7da1a74
tgbbb43 <tgbbb43@gmail.com>
2026-03-29 10:45:16 +0300

    add for loop

---
+++ loops/for_loops.py
@@ -0,0 +1,13 @@
+
+     
+   # friends = ["Rolf", "Jen", "Bob", "Anne"]
+
+# for friend in friends:
+#      print(f"{friend} is my friend")
+
+# range()
+# for number in range(4):
+    # print(f"{number} is my friend")
+
+for number in range(0, 1000000, 3):
+    print(f"{number} is my friend")  
