adc3f234ccbfd47592da54d867c3543d781e56a2
tgbbb43 <tgbbb43@gmail.com>
2026-03-29 11:58:14 +0300

    Uploading Loop Labs

--- loops/for_loops.py
+++ loops/for_loops.py
@@ -9,5 +9,61 @@
 # for number in range(4):
     # print(f"{number} is my friend")
 
-for number in range(0, 1000000, 3):
-    print(f"{number} is my friend")  
+# for number in range(0, 1000000, 3):
+    # print(f"{number} is my friend")  
+    
+#? [Python Basics] Class-Lab 4.5    
+#  1
+# for i in range(6):
+#     print(i)
+    
+# 2 
+# for i in range(5, 0, -1):
+#     print(i)
+
+# 3
+# for i in range(0, 11, 2):
+#     print(i)
+ 
+# 4
+# for i in range(11):
+#     print(i) 
+
+# 5 
+
+# for i in range(3,21,3):
+#     print(i) 
+
+# 6
+# for i in range(5):
+    # print("hello")
+    
+# 7 
+# for i in range(0,10):
+#     print(i)    
+
+# 8
+# for i in range(2,21,2):
+#     print(i) 
+
+# 9
+# for i in range(10,-1,-1):
+#     print(i) 
+
+# 10
+# t = 0
+# for i in range(1,101):
+#   t += i
+#   print(t)
+
+# 11
+# nemas = ["eli","ben","dan","aviv"]
+# for nema in nemas:
+#     print(nema)
+
+# 12
+nemas = ["eli","ben","dan","aviv"]
+count = 0
+for nema in nemas:
+    count += 1
+    print(count)
--- loops/whlile_loops.py
+++ loops/whlile_loops.py
@@ -7,9 +7,65 @@
     # user_input = input("Would you like to play again (n/y)")
     
     
-while True:
-    user_input = input("Would you like to paly? (n/y)")
+# while True:
+#     user_input = input("Would you like to paly? (n/y)")
     
-    if user_input == "n":
-        break
-    print("Game is runing")
+#     if user_input == "n":
+#         break
+#     print("Game is runing")
+
+#?  [Python Basics] Hands-On Lab 4.0
+# 1
+# i = 1
+# while i <= 5:
+#     print(i)
+#     i += 1
+    # 
+# 2 
+
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
+ 
+# 3 
+
+# i = 2
+# while i <= 10:
+#     print(i)
+#     i += 2   
+
+# 4 
+
+# i = 1
+# total = 0
+# while i <= 5:
+#     total += i
+#     i += 1
+# print("Sum:", total)
+
+# 5
+
+# i = 3
+# while i <= 10:
+#     print(i)
+#     i += 3
+
+# 6
+
+# user_in = input("enter a number ")
+
+# i = 1
+# while i <= 5:
+#      print(user_in)
+#      i += 1
+     
+# 7 
+user_in = int(input("enter a number "))
+i = 1
+while i <= user_in:
+    print(i)
+    i += 1
+
+    
+   
